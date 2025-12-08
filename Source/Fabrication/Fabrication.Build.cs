27a50fd57e45989983e9155680699d35e98a8b33
손우참(Unreal5기) <dncka22@naver.com>
2025-12-08 16:42:28 +0900

    Update Fabrication.Build.cs

--- Source/Fabrication/Fabrication.Build.cs
+++ Source/Fabrication/Fabrication.Build.cs
@@ -8,16 +8,38 @@ public class Fabrication : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+            "Core", 
+            "CoreUObject", 
+            "Engine", 
+            "InputCore", 
+            "EnhancedInput",  
+            "AIModule",
+            "NavigationSystem",
+            "GameplayTasks",
+            "UMG",                
+            "Slate",     
+            "SlateCore",
+            "OnlineSubsystem",
+            "OnlineSubsystemUtils",
+            "NetCore",     
+            "Json", 
+            "JsonUtilities",
+            "HTTP"
+        });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicIncludePaths.AddRange(new string[]
+        {
+            "Fabrication",
+		});
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
