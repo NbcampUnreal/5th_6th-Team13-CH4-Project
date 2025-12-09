7f13a52487df9e9f8c33eded4125d1ad8542d45d
SuhyunPark <xjcl2058@gmail.com>
2025-12-09 14:34:15 +0900

    Squashed commit of the following:
    commit 5b791a1441693121e5cbe83062ce81a72f8925ef
    Author: SuhyunPark <xjcl2058@gmail.com>
    Date:   Tue Dec 9 14:32:44 2025 +0900
    
        update_0
    
        datatable 생성, eventmanger

---
+++ Source/Fabrication/Event/LevelEventManager.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Engine/DataTable.h"
+#include "LevelEventManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FABRICATION_API ULevelEventManager : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	ULevelEventManager();
+
+	void StartEventLoop();
+
+	void TriggerRandomEvent();
+
+	void SetEmergencyMode(bool bActive);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Event")
+	UDataTable* HazardDataTable;
+	
+};
