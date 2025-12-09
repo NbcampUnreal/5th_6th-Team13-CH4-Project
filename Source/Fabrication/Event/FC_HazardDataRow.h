5b791a1441693121e5cbe83062ce81a72f8925ef
SuhyunPark <xjcl2058@gmail.com>
2025-12-09 14:32:44 +0900

    update_0
    datatable 생성, eventmanger

---
+++ Source/Fabrication/Event/FC_HazardDataRow.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "FC_HazardDataRow.generated.h"
+
+UENUM(BlueprintType)
+enum class EHazardType : uint8
+{
+    Garden,
+    Kitchen,
+    LobbyVision,
+    Painting,
+    Dining,
+    Door,
+    Bell
+};
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FC_HazardDataRow : public FTableRowBase
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    EHazardType HazardType;
+
+    // Hazard zone location or area
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FVector Location;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float TriggerRadius;
+
+    // Damage over time
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float DamageAmount;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float DamageDelay;
+
+    // Special parameters (multi-use)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SpecialParam1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SpecialParam2;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString Description;
+};
