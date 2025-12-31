f86b730014c57d9b3689e561ddb7be3d6802fc22
ggyu-ik <ggye0723@gmail.com>
2025-12-31 15:44:48 +0900

    update Waiting에서 Playing으로 넘어갈 때 순간이동 하도록 설정

--- Source/Fabrication/GameMode/FCGameMode.h
+++ Source/Fabrication/GameMode/FCGameMode.h
@@ -60,6 +60,13 @@ protected:
 	int32 EndingTimeLimit;
 
 	int32 RemainEndingTime;
+
+	// 플레이어 순간이동 위치 (블루프린트에서 설정 가능)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Teleport", meta = (AllowPrivateAccess))
+	FVector PlayerTeleportLocation = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Teleport", meta = (AllowPrivateAccess))
+	FRotator PlayerTeleportRotation = FRotator::ZeroRotator;
 	
 private:
 	UFUNCTION()
@@ -83,6 +90,10 @@ private:
 
 public:
 	USpawnManager* GetSpawnManger();
+
+	// 플레이어들을 특정 위치로 순간이동시키는 함수
+	UFUNCTION(BlueprintCallable, Category = "Player Teleport")
+	void TeleportAllPlayersToLocation(const FVector& Location, const FRotator& Rotation = FRotator::ZeroRotator);
 
 private:
 	void StartSpawn();
